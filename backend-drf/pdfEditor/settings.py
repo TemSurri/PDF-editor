d24e9107dca9f62c433c55df18dd8a2604233999
tembatsurri <tarantem@icloud.com>
2025-10-02 21:34:17 -0700

    debug false

--- backend-drf/pdfEditor/settings.py
+++ backend-drf/pdfEditor/settings.py
@@ -39,7 +39,7 @@ REST_FRAMEWORK = {
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend", "frontend"]
 
