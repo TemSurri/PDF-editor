0ccd709afa99f988eb2ecefd230fc9e075a3a6fe
tembatsurri <tarantem@icloud.com>
2025-10-14 16:19:11 -0700

    switched to postgres

--- backend-drf/pdfEditor/settings.py
+++ backend-drf/pdfEditor/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from decouple import config
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
@@ -95,10 +96,11 @@ WSGI_APPLICATION = 'pdfEditor.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+            config('DB_URL'), 
+            conn_max_age = 600,
+            ssl_require = True
+            )
 }
 
 
