4c0c8caea3bc3f2165057cdf8217cd8bc00624c9
tembatsurri <tarantem@icloud.com>
2025-10-16 21:17:35 -0700

    removed all supabase connections

--- backend-drf/pdfEditor/wsgi.py
+++ backend-drf/pdfEditor/wsgi.py
@@ -7,15 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import socket
 import os 
 
-try:
-    s = socket.create_connection(("aws-1-us-east-2.pooler.supabase.com", 5432), timeout=5)
-    print("✅ Supabase is reachable")
-    s.close()
-except Exception as e:
-    print("❌ Supabase unreachable:", e)
+
 
 
 from django.core.wsgi import get_wsgi_application
