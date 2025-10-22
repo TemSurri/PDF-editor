8d6c94b499bccfaf3a772de716127e1dc563f058
tembatsurri <tarantem@icloud.com>
2025-10-21 19:53:58 -0700

    reduced token lifetime

--- backend-drf/pdfEditor/settings.py
+++ backend-drf/pdfEditor/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 import dj_database_url
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=0.25),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
